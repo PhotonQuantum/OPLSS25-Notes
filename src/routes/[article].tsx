b5a19a7cc672d087337292032d7bc3d51d03cedb
LightQuantum <self@lightquantum.me>
2025-07-13 01:56:22 -0400

    fix: glyphs missing when switching between artifacts

--- src/routes/[article].tsx
+++ src/routes/[article].tsx
@@ -1,8 +1,9 @@
 import { createResizeObserver } from "@solid-primitives/resize-observer";
 import { useLocation, useParams } from "@solidjs/router";
 import { createEffect, createResource, createSignal, onMount } from "solid-js";
+import { Dynamic } from "solid-js/web";
 import { metaJsons, typstArtifacts } from "~/assets/typst";
-import Typst from "~/components/Typst";
+import Typst, { TypstProps } from "~/components/Typst";
 import { usePageTitle } from "~/context/title";
 import { convertMetaToLocationMap } from "~/typst/meta";
 
@@ -32,12 +33,28 @@ export default function Article() {
     })
   })
 
+  // NOTE this is due to a bug in typst.ts. Glyphs are not reloaded when the artifact changes.
+  // We are forced to create a new component instance when the artifact changes.
+  const TypstComponent = () => {
+    if (artifactUrl() === undefined) {
+      // Avoid creating typst component when artifact is undefined
+      return () => <div />
+    } else {
+      // eta expand it to avoid component caching
+      return (props: TypstProps) => Typst(props)
+    }
+  }
+
   return (
     <>
       <div class="absolute w-full h-full -z-50" ref={measureRef} />
-      <Typst artifact={artifactUrl()} scrollMargin={{
-        top: () => `${scrollMarginTop()}px`,
-      }} locationMap={locationMap()} jumpKey={jumpKey}/>
+      <Dynamic component={TypstComponent()}
+        artifact={artifactUrl()}
+        scrollMargin={{
+          top: () => `${scrollMarginTop()}px`,
+        }}
+        locationMap={locationMap()}
+        jumpKey={jumpKey} />
     </>
   );
 }
